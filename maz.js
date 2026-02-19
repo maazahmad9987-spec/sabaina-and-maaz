b622c7308c9d4911837c76322a90bca029c2c960
maazahmad9987-spec <maazahmad9987@gmail.com>
2026-02-19 22:51:24 +0500

    Add files via upload

---
+++ maz.js
@@ -0,0 +1,22 @@
+// SET YOUR BIRTHDAY DATE HERE
+var birthday = new Date("Feb 20, 2026 00:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+  var distance = birthday - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "🎉 It's Birthday Time! 🎉";
+  }
+
+}, 1000);
